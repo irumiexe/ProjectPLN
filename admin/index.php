5895a1cb7dc9b1ac2be8d386dacc41dac472375f
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-09-27 17:09:34 +0800

    itu

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,19 @@
 <?php
-    include'header.php';
+include 'header.php';
 ?>
 
 <div class="container">
     <div class="row">
-        <ol class="breadcrumb"> <h4>DASHBOARD</h4></ol>
+        <ol class="breadcrumb">
+            <h4>DASHBOARD</h4>
+        </ol>
     </div>
     <div class="panel-container">
         <div class="bootstrap-tabel">
             <center>
-                <h3>SELAMAT DATANG DI WEB SEDERHANA</h3>
+                <h3>SELAMAT DATANG DI WEB</h3>
             </center>
-            
+
         </div>
 
     </div>
