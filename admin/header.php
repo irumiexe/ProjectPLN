066248f35cfface2c0acadd11b2177d0f3d32b38
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-09-29 11:40:34 +0800

    header

--- admin/header.php
+++ admin/header.php
@@ -4,28 +4,47 @@ include '../assets/conn/cek.php';
 include '../assets/conn/config.php';
 ?>
 
-<html>
+<html lang="en">
 
 <head>
     <title>Web Sederhana</title>
-    <link rel="stylesheet" href="../assets/css/cosmo.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Menggunakan Bootstrap CSS dari CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/cosmo.min.css"> <!-- Jangan lupa tautan CSS kosmo jika diperlukan -->
 </head>
 
-<body onload="getLocation();">
-    <nav class="navbar-inverse navbar-static-right">
+<body onload="getLocation()">
 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href=""></a></li>
-                    <li><a href="index.php">DASHBOARD</a></li>
-                    <li><a href="pelangganinput.php">DATA PELANGGAN</a></li>
-                    <li><a href="logout.php">LOGOUT</a></li>
+            <a class="navbar-brand" href="#">Web Sederhana</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">DASHBOARD</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pelangganinput.php">DATA PELANGGAN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">LOGOUT</a>
+                    </li>
                 </ul>
             </div>
         </div>
-
     </nav>
+
+    <!-- Isi konten halaman di sini -->
+
+    <!-- Menggunakan Bootstrap JS (jQuery, Popper.js, dan Bootstrap JS) dari CDN -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.8/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
