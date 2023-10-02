304dc09c65185422fbb03aa05166f39930da1295
irumiexe <muhammadilmi2309@gmail.com>
2023-10-02 15:59:38 +0800

    r

--- admin/header.php
+++ admin/header.php
@@ -15,6 +15,7 @@ include '../assets/conn/config.php';
     <!-- Menggunakan Bootstrap CSS dari CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/cosmo.min.css">
+    <link rel="stylesheet" href="../assets/css/css_final.css">
 
     <style>
         .navbar-brand {
