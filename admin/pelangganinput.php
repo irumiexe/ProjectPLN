6d55c4fce8297952ccb3415dbec90646366d5e1b
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-10-02 16:38:48 +0800

    tes

--- admin/pelangganinput.php
+++ admin/pelangganinput.php
@@ -2,7 +2,7 @@
 include 'header.php';
 ?>
 
-<div class="container-xl ">
+<div class="container">
     <div class="row">
         <ol class="breadcrumb">
             <h4>INPUT DATA PELANGGAN</h4>
@@ -20,9 +20,9 @@ include 'header.php';
                             <th class="text-center">Nama Pelanggan</th>
                             <th class="text-center">Daya</th>
                             <th class="text-center">Tipe Pembayaran</th>
-                            <th class="text-center" colspan="">Maps</th>
-                            <th class="text-center" colspan="">Photo Meteran</th>
-                            <th class="text-center">Keterangan</th>
+                            <th class="text-center">Maps</th>
+                            <th class="text-center">Photo Meteran</th>
+                            <th class="text-center">Keterangann</th>
                             <th class="text-center">Opsi</th>
                         </tr>
                     </thead>
@@ -37,7 +37,9 @@ include 'header.php';
                                 <td class="text-center"><?php echo $d['nama_pel'] ?></td>
                                 <td class="text-center"><?php echo $d['daya'] ?></td>
                                 <td class="text-center"><?php echo $d['tipe'] ?></td>
-                                <td style="width: 450px; height : 450px;" colspan="2"> <iframe src='https://www.google.com/maps?q=<?Php echo $d["latitude"] ?>,<?php echo $d["longitude"]; ?>&hl=es;z=14&output=embed' style="width:100%;  height:100%; "></iframe> </td>
+                                <td style="width: 450px; height: 450px;">
+                                    <iframe src='https://www.google.com/maps?q=<?Php echo $d["latitude"] ?>,<?php echo $d["longitude"]; ?>&hl=es;z=14&output=embed' style="width:100%; height:100%;"></iframe>
+                                </td>
                                 <td class="text-center"><img src="../file/<?php echo $d['pmet']; ?>" style="width: 100px;"></td>
                                 <td class="text-center"><?php echo $d['ket'] ?></td>
                                 <td class="text-center">
