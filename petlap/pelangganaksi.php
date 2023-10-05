9462df448f5294d7e9f52676fb02a2137e3dcfff
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-10-05 15:03:29 +0800

    ariadi

--- petlap/pelangganaksi.php
+++ petlap/pelangganaksi.php
@@ -1,33 +1,17 @@
 <?php
 include 'header.php';
+
+// Pastikan user sudah login dan ada informasi kd_akun_user di sesi
+if (!isset($_SESSION['kd_akun_user'])) {
+    // Jika tidak, mungkin redirect ke halaman login atau lakukan tindakan lain
+    header("Location: login.php");
+    exit();
+}
+
 if (isset($_GET['aksi'])) {
     if ($_GET['aksi'] == 'tambah') {
-        if (isset($_POST["submit"])) {
-            $idpel = $_POST['idpel'];
-            $nama_pel = $_POST['nama_pel'];
-            $daya = $_POST['daya'];
-            $tipe = $_POST['tipe'];
-            $latitude = $_POST["latitude"];
-            $longitude = $_POST["longitude"];
-
-            $pmet = $_FILES['pmet']['name'];
-            move_uploaded_file($_FILES['pmet']['tmp_name'], '../file/' . $_FILES['pmet']['name']);
-
-            $ket = $_POST["ket"];
-            $query = "INSERT INTO tbl_pelanggan (idpel, nama_pel, daya, tipe, latitude, longitude, pmet, ket, tanggal) 
-            VALUES ('$idpel', '$nama_pel', '$daya', '$tipe', '$latitude', '$longitude', '$pmet', '$ket', CURDATE())";
-
-            mysqli_query($db, $query);
-
-            echo
-            "
-            <script>
-            alert('Data Berhasil Di Tambahkan');
-            document.location.href = 'pelangganinput.php';
-            </script>
-            ";
-        }
-
+        // Mengambil kd_akun_user dari sesi
+        $kd_akun_user = $_SESSION['kd_akun_user'];
 ?>
 
         <div class="container">
@@ -37,14 +21,9 @@ if (isset($_GET['aksi'])) {
                 </ol>
             </div>
 
-            <?php
-
-            $carikode = $db->query("SELECT max(idpel) FROM tbl_pelanggan");
-            ?>
-
             <div class="panel-container">
                 <div class="bootstrap-tabel">
-                    <form class="myForm" action="" method="post" autocomplete="off" enctype="multipart/form-data">
+                    <form class="myForm" action="pelangganproses.php?proses=prosestambah" method="post" autocomplete="off" enctype="multipart/form-data">
                         <div class="form-group">
                             <label for="">ID Pelanggan</label>
                             <input type="number" name="idpel" class="form-control" value="" placeholder="id pelanggan">
@@ -91,6 +70,10 @@ if (isset($_GET['aksi'])) {
                             <label for="">Tanggal</label>
                             <input type="text" name="tanggal" class="form-control" value="<?php echo date('Y-m-d'); ?>" readonly>
                         </div>
+                        <div class="form-group">
+                            <label for="">kode_akun</label>
+                            <input type="text" name="kd_akun" class="form-control" value="<?php echo $kd_akun_user; ?>" readonly>
+                        </div>
                         <div class="modal-footer">
                             <a href="pelangganinput.php" class="btn btn-primary">Kembali</a>
                             <button type="submit" class="btn btn-success" name="submit"> Submit</button>
@@ -120,7 +103,76 @@ if (isset($_GET['aksi'])) {
                             }
                         }
                     </script>
+                </div>
+            </div>
+        </div>
+    <?php } elseif ($_GET['aksi'] == 'ubah') { ?>
+        <div class="container">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <h4>PELANGGAN/ UBAH</h4>
+                </ol>
+            </div>
 
+            <div class="panel-container">
+                <div class="bootstrap-tabel">
+                    <?php
+                    $data = $db->query("SELECT * From tbl_pelanggan where idpel='$_GET[kode]'");
+                    while ($d = mysqli_fetch_array($data)) {
+                    ?>
+                        <form action="pelangganproses.php?proses=ubah" method="post" enctype="multipart/form-data">
+                            <div>
+                                <label for="">ID Pelanggan</label>
+                                <input type="text" name="idpel" class="form-control" value="<?php echo $d['idpel'] ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Nama Pelanggan</label>
+                                <input type="text" name="nama_pel" class="form-control" value="<?php echo $d['nama_pel'] ?>" placeholder="nama pelanggan">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Daya</label>
+                                <select name="daya" id="" class="form-control">
+                                    <option value="450VA">450VA</option>
+                                    <option value="900VA">900VA</option>
+                                    <option value="1300VA">1300VA</option>
+                                    <option value="2200VA">2200VA</option>
+                                    <option value="3500VA">3500VA</option>
+                                    <option value="6600VA">6600VA</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Tipe Pembayaran</label>
+                                <select name="tipe" id="" class="form-control">
+                                    <option value=<?php echo $d['tipe'] ?>""><?php echo $d['tipe'] ?></option>
+                                    <option value="Pascabayar">Pascabayar</option>
+                                    <option value="Prabayar">Prabayar</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Lokasi</label>
+                                <tr>
+                                    <td><input type="text" name="latitude" class="form-control" value="<?php echo $d['latitude'] ?>"></td>
+                                    <td><input type="text" name="longitude" class="form-control" value="<?php echo $d['longitude'] ?>"></td>
+                                </tr>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Photo Meteran</label>
+                                <input type="file" name="pmet" class="form-control" value="<?php echo $d['pmet'] ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Keterangan</label>
+                                <input type="text" name="ket" class="form-control" value="<?php echo $d['ket'] ?>" placeholder="keterangan">
+                            </div>
+                            <div class="form-group">
+                                <label for="">kode_akun</label>
+                                <input type="text" name="kd_akun" class="form-control" value="<?php echo $d['kd_akun']; ?>" readonly>
+                            </div>
+                            <div class="modal-footer">
+                                <a href="pelangganinput.php" class="btn btn-primary">Kembali</a>
+                                <input type="submit" class="btn btn-success" value="Ubah">
+                            </div>
+                        </form>
+                    <?php } ?>
                 </div>
             </div>
         </div>
--- petlap/pelangganproses.php
+++ petlap/pelangganproses.php
@@ -6,9 +6,43 @@ if (isset($_GET['proses'])) {
         $nama_pel = $_POST['nama_pel'];
         $daya = $_POST['daya'];
         $tipe = $_POST['tipe'];
+        $latitude = $_POST["latitude"];
+        $longitude = $_POST["longitude"];
+        $pmet = $_FILES['pmet']['name'];
 
-        $hasil = $db->query("INSERT into tbl_pelanggan (idpel,nama_pel,daya,tipe) values
-                                                            ('$idpel','$nama_pel','$daya','$tipe')");
+        move_uploaded_file($_FILES['pmet']['tmp_name'], '../file/' . $_FILES['pmet']['name']);
+        $ket = $_POST["ket"];
+        $kd_akun = $_POST['kd_akun'];  // Tambahkan baris ini
+
+        $query = "INSERT INTO tbl_pelanggan (idpel, nama_pel, daya, tipe, latitude, longitude, pmet, ket, tanggal, kd_akun) 
+          VALUES ('$idpel', '$nama_pel', '$daya', '$tipe', '$latitude', '$longitude', '$pmet', '$ket', CURDATE(), '$kd_akun')";
+
+        mysqli_query($db, $query);
+
+        echo
+        "<script>
+            alert('Data Berhasil Di Tambahkan');
+            document.location.href = 'pelangganinput.php';
+        </script>";
         header("location:pelangganinput.php");
-    } 
+    } elseif ($_GET['proses'] == 'ubah') {
+        $idpel = $_POST['idpel'];
+        $nama_pel = $_POST['nama_pel'];
+        $daya = $_POST['daya'];
+        $tipe = $_POST['tipe'];
+        $latitude = $_POST["latitude"];
+        $longitude = $_POST["longitude"];
+        $pmet = $_FILES['pmet']['name'];
+
+        $pmet = $_FILES['pmet']['name'];
+        move_uploaded_file($_FILES['pmet']['tmp_name'], '../file/' . $_FILES['pmet']['name']);
+        $ket = $_POST["ket"];
+
+        $hasil = $db->query("UPDATE tbl_pelanggan set nama_pel='$nama_pel', daya='$daya', tipe='$tipe',pmet='$pmet',ket='$ket' where idpel='$idpel'");
+        header("location:pelangganinput.php");
+    } elseif ($_GET['proses'] == 'proseshapus') {
+        $idpel = $_GET['kode'];
+        $hasil = $db->query("DELETE FROM tbl_pelanggan WHERE idpel='$idpel'");
+        header("location:pelangganinput.php");
+    }
 }
