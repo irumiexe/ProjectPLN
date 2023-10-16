9ffb0bf0f06a46f79c567e3a98ba3f29f65affd4
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-10-16 15:14:21 +0800

    ariadi

--- admin/pelangganinput.php
+++ admin/pelangganinput.php
@@ -1,7 +1,9 @@
 <?php
 include 'header.php';
-?>
 
+$dataPerPage = 1;
+
+?>
 <style>
     .excel-btn {
         padding: 10px 20px;
@@ -11,17 +13,6 @@ include 'header.php';
         font-size: 16px;
         margin-top: 10px;
         transition: background-color 0.2s ease;
-    }
-
-    .pagination1 {
-        text-align: center;
-        margin-top: 20px;
-        /* Atur sesuai kebutuhan Anda */
-    }
-
-    .pagination1 a {
-        margin: 0 1px;
-        /* Atur spasi antara tombol pagination */
     }
 </style>
 
@@ -34,22 +25,28 @@ include 'header.php';
     <div class="panel-container">
         <div class="bootstrap-tabel">
             <div class="d-flex justify-content-between mb-3">
-                <a href="pelangganaksi.php?aksi=tambah" class="btn btn-primary">Tambah Data</a>
-
+                <div class="row">
+                    <div class="col">
+                        <a href="pelangganaksi.php?aksi=tambah" class="btn btn-primary">Tambah Data</a>
+                    </div>
+                    <div class="col">
+                        <a href="excel.php" target="_blank">
+                            <button class="btn btn-success">Excel</button>
+                        </a>
+                    </div>
+                </div>
             </div>
-
-
             <div class="card">
-                <div class=" d-flex justify-content-between mb-3 card-header">
-                    <h4 class=" card-title ">Data Pelanggan</h4>
-                    <form class="d-flex ml-auto">
-                        <input class="form-control mr-2" name="cari" type="search" placeholder="Search" aria-label="Search" value="<?php if (isset($_GET['cari'])) {
+                <div class="d-flex justify-content-between mb-3 card-header">
+                    <h4 class="card-title">Data Pelanggan</h4>
+                    <form class="d-flex ml-auto" method="GET">
+                        <input class="form-control mr-1" name="cari" type="search" placeholder="Search" aria-label="Search" value="<?php if (isset($_GET['cari'])) {
                                                                                                                                         echo $_GET['cari'];
                                                                                                                                     } ?>">
-                        <button class="btn btn-outline-success" type="cari">Search</button>
+                        <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
-                <div class=" mx-3 table-responsive">
+                <div class="mx-3 table-responsive">
                     <table class="table table-bordered">
                         <thead>
                             <tr>
@@ -71,7 +68,6 @@ include 'header.php';
                                 $hasil = "SELECT * from tbl_pelanggan where idpel like '%" . $pencarian . "%' or nama_pel like '%" . $pencarian . "%' 
                                                                             or ket like '%" . $pencarian . "%' or daya like '%" . $pencarian . "%' or tipe like '%" . $pencarian . "%' order by idpel asc";
                             } else {
-                                $dataPerPage = 5; // Jumlah data per halaman
                                 $currentPage = isset($_GET['page']) ? $_GET['page'] : 1; // Halaman saat ini, default: 1
                                 $startFrom = ($currentPage - 1) * $dataPerPage; // Mulai dari data ke berapa
 
@@ -103,51 +99,64 @@ include 'header.php';
                                         <a href="pelangganaksi.php?kode=<?php echo $d['kd_idpel'] ?>&aksi=ubah" class="btn btn-success">Ubah</a>
                                         <a href="javascript:void(0);" class="btn btn-danger" onclick="hapusData('<?php echo $d['kd_idpel']; ?>')">Hapus</a>
                                     </td>
-
                                 </tr>
                             <?php
                             }
                             ?>
                         </tbody>
                     </table>
-                    <div class="pagination1">
-                        <?php
-                        $query = "SELECT COUNT(*) AS total FROM tbl_pelanggan";
-                        $result = mysqli_query($db, $query);
-                        $row = mysqli_fetch_assoc($result);
-                        $totalPages = ceil($row['total'] / $dataPerPage);
-                        $startRange = max(1, $currentPage - 2);
-                        $endRange = min($totalPages, $currentPage + 2);
+                </div>
+                <div class="pagination">
+                    <?php
+                    $query = "SELECT COUNT(*) AS total FROM tbl_pelanggan";
+                    if (isset($_GET['cari'])) {
+                        $pencarian = $_GET['cari'];
+                        $query .= " WHERE idpel LIKE '%$pencarian%' OR nama_pel LIKE '%$pencarian%' OR ket LIKE '%$pencarian%' OR daya LIKE '%$pencarian%' OR tipe LIKE '%$pencarian%'";
+                    }
+                    $result = mysqli_query($db, $query);
+                    $row = mysqli_fetch_assoc($result);
+                    $totalPages = ($row['total'] > 0) ? ceil($row['total'] / $dataPerPage) : 1;
+                    $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+                    $startFrom = ($currentPage - 1) * $dataPerPage; // Mulai dari data ke berapa
+
+                    // Tambahkan LIMIT ke dalam query data
+                    $queryData = "SELECT * FROM tbl_pelanggan";
+                    if (isset($_GET['cari'])) {
+                        $queryData .= " WHERE idpel LIKE '%$pencarian%' OR nama_pel LIKE '%$pencarian%' OR ket LIKE '%$pencarian%' OR daya LIKE '%$pencarian%' OR tipe LIKE '%$pencarian%'";
+                    }
+                    $queryData .= " ORDER BY idpel ASC LIMIT $startFrom, $dataPerPage";
+
+                    $resultData = mysqli_query($db, $queryData);
+                    while ($d = $resultData->fetch_array()) {
+                        // Tampilkan data sesuai dengan kebutuhan
+                    }
+
+                    // Tampilkan tombol pagination
+                    if ($totalPages > 1) {
+                        echo '<nav aria-label="Page navigation example">';
+                        echo '<ul class="pagination">';
                         if ($currentPage > 1) {
-                            echo '<a href="?page=' . ($currentPage - 1) . '" class="btn btn-primary">&laquo;</a>';
+                            echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage - 1) . '">&laquo;</a></li>';
                         }
-
-                        if ($currentPage > 3) {
-                            echo '<a href="?page=1" class="btn btn-primary">1</a>';
-                            echo '<span class="btn btn-secondary">...</span>';
+                        for ($i = 1; $i <= $totalPages; $i++) {
+                            echo '<li class="page-item ' . (($i == $currentPage) ? 'active' : '') . '"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
                         }
-                        for ($i = $startRange; $i <= $endRange; $i++) {
-                            echo '<a href="?page=' . $i . '" class="btn ' . (($i == $currentPage) ? 'btn-info' : 'btn-secondary') . '">' . $i . '</a>';
-                        }
-                        if ($currentPage < $totalPages - 2) {
-                            echo '<span class="btn btn-secondary">...</span>';
-                            echo '<a href="?page=' . $totalPages . '" class="btn btn-primary">' . $totalPages . '</a>';
-                        }
-
                         if ($currentPage < $totalPages) {
-                            echo '<a href="?page=' . ($currentPage + 1) . '" class="btn btn-primary">&raquo;</a>';
+                            echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage + 1) . '">&raquo;</a></li>';
                         }
-                        ?>
-                    </div>
+                        echo '</ul>';
+                        echo '</nav>';
+                    }
+                    ?>
                 </div>
             </div>
         </div>
     </div>
-
-    <script>
-        function hapusData(idpelanggan) {
-            if (confirm('Apakah Anda yakin ingin menghapus data ini?')) {
-                window.location.href = 'pelangganproses.php?kode=' + idpelanggan + '&proses=proseshapus';
-            }
+</div>
+<script>
+    function hapusData(idpelanggan) {
+        if (confirm('Apakah Anda yakin ingin menghapus data ini?')) {
+            window.location.href = 'pelangganproses.php?kode=' + idpelanggan + '&proses=proseshapus';
         }
-    </script>
+    }
+</script>
