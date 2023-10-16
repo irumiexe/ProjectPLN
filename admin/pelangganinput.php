34170e5a5ef0753eb194f0bcd8b37d469a5c88e9
irumiexe <muhammadilmi2309@gmail.com>
2023-10-16 16:00:28 +0800

    ilmi

--- admin/pelangganinput.php
+++ admin/pelangganinput.php
@@ -23,6 +23,55 @@ $dataPerPage = 2;
     .pagination a {
         margin: 0 1px;
     }
+
+    .modal {
+        display: none;
+        position: fixed;
+        z-index: 1;
+        padding-top: 100px;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgba(0, 0, 0, 0.9);
+    }
+
+    .modal-header {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: start;
+        align-items: flex-start;
+        -ms-flex-pack: justify;
+        justify-content: space-between;
+        padding: 1rem;
+        border-bottom: 1px solid #e9ecef;
+        border-top-left-radius: calc(0.3rem - 1px);
+        border-top-right-radius: calc(0.3rem - 1px);
+    }
+
+    .modal-content {
+        display: block;
+        margin: 0 auto;
+        max-width: 30%;
+    }
+
+    .modal-body {
+        position: relative;
+        -ms-flex: 1 1 auto;
+        flex: 1 1 auto;
+        padding: 1rem;
+    }
+
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        color: #fff;
+        font-size: 24px;
+        font-weight: bold;
+        cursor: pointer;
+    }
 </style>
 
 <div class="container-xl">
@@ -97,7 +146,11 @@ $dataPerPage = 2;
                                     <td style="width: 200px; height: 200px;">
                                         <iframe src='https://www.google.com/maps?q=<?Php echo $d["latitude"] ?>,<?php echo $d["longitude"]; ?>&hl=es;z=14&output=embed' style="width:100%; height:100%;"></iframe>
                                     </td>
-                                    <td class="text-center"><img src="../file/<?php echo $d['pmet']; ?>" style="width: 100px; height:200px"></td>
+                                    <td class="text-center">
+                                        <a href="javascript:void(0);" onclick="tampilkanGambar('../file/<?php echo $d['pmet']; ?>')">
+                                            <img src="../file/<?php echo $d['pmet']; ?>" style="width: 100px; height: 200px">
+                                        </a>
+                                    </td>
                                     <td class="text-center"><?php echo $d['ket'] ?></td>
                                     <td class="text-center" style="max-width: 100px;">
                                         <div style="word-wrap: break-word; ">
@@ -114,55 +167,86 @@ $dataPerPage = 2;
                             ?>
                         </tbody>
                     </table>
-                </div>
-                <div class="pagination">
-                    <?php
-                    $query = "SELECT COUNT(*) AS total FROM tbl_pelanggan";
-                    if (isset($_GET['cari'])) {
-                        $pencarian = $_GET['cari'];
-                        $query .= " WHERE idpel LIKE '%$pencarian%' OR nama_pel LIKE '%$pencarian%' OR ket LIKE '%$pencarian%' OR daya LIKE '%$pencarian%' OR tipe LIKE '%$pencarian%'";
-                    }
-                    $result = mysqli_query($db, $query);
-                    $row = mysqli_fetch_assoc($result);
-                    $totalPages = ($row['total'] > 0) ? ceil($row['total'] / $dataPerPage) : 1;
-                    $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
-                    $startFrom = ($currentPage - 1) * $dataPerPage; // Mulai dari data ke berapa
+                    <div id="gambarPopUp" class="modal">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h3>Bukti Dokumentasi</h3>
+                                <span class="close" onclick="tutupPopUp()">&times;</span>
+                            </div>
+                            <div class="modal-body">
+                                <img id="gambarModal" style="width: 100%; item-align: center;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pagination">
+                        <?php
+                        $query = "SELECT COUNT(*) AS total FROM tbl_pelanggan";
+                        if (isset($_GET['cari'])) {
+                            $pencarian = $_GET['cari'];
+                            $query .= " WHERE idpel LIKE '%$pencarian%' OR nama_pel LIKE '%$pencarian%' OR ket LIKE '%$pencarian%' OR daya LIKE '%$pencarian%' OR tipe LIKE '%$pencarian%'";
+                        }
+                        $result = mysqli_query($db, $query);
+                        $row = mysqli_fetch_assoc($result);
+                        $totalPages = ($row['total'] > 0) ? ceil($row['total'] / $dataPerPage) : 1;
+                        $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+                        $startFrom = ($currentPage - 1) * $dataPerPage; // Mulai dari data ke berapa
 
-                    // Tambahkan LIMIT ke dalam query data
-                    $queryData = "SELECT * FROM tbl_pelanggan";
-                    if (isset($_GET['cari'])) {
-                        $queryData .= " WHERE idpel LIKE '%$pencarian%' OR nama_pel LIKE '%$pencarian%' OR ket LIKE '%$pencarian%' OR daya LIKE '%$pencarian%' OR tipe LIKE '%$pencarian%'";
-                    }
-                    $queryData .= " ORDER BY idpel ASC LIMIT $startFrom, $dataPerPage";
+                        // Tambahkan LIMIT ke dalam query data
+                        $queryData = "SELECT * FROM tbl_pelanggan";
+                        if (isset($_GET['cari'])) {
+                            $queryData .= " WHERE idpel LIKE '%$pencarian%' OR nama_pel LIKE '%$pencarian%' OR ket LIKE '%$pencarian%' OR daya LIKE '%$pencarian%' OR tipe LIKE '%$pencarian%'";
+                        }
+                        $queryData .= " ORDER BY idpel ASC LIMIT $startFrom, $dataPerPage";
 
-                    $resultData = mysqli_query($db, $queryData);
-                    while ($d = $resultData->fetch_array()) {
-                    }
-                    if ($totalPages > 1) {
-                        echo '<nav aria-label="Page navigation example">';
-                        echo '<ul class="pagination">';
-                        if ($currentPage > 1) {
-                            echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage - 1) . '">&laquo;</a></li>';
+                        $resultData = mysqli_query($db, $queryData);
+                        while ($d = $resultData->fetch_array()) {
                         }
-                        for ($i = 1; $i <= $totalPages; $i++) {
-                            echo '<li class="page-item ' . (($i == $currentPage) ? 'active' : '') . '"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+                        if ($totalPages > 1) {
+                            echo '<nav aria-label="Page navigation example">';
+                            echo '<ul class="pagination">';
+                            if ($currentPage > 1) {
+                                echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage - 1) . '">&laquo;</a></li>';
+                            }
+                            for ($i = 1; $i <= $totalPages; $i++) {
+                                echo '<li class="page-item ' . (($i == $currentPage) ? 'active' : '') . '"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+                            }
+                            if ($currentPage < $totalPages) {
+                                echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage + 1) . '">&raquo;</a></li>';
+                            }
+                            echo '</ul>';
+                            echo '</nav>';
                         }
-                        if ($currentPage < $totalPages) {
-                            echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage + 1) . '">&raquo;</a></li>';
-                        }
-                        echo '</ul>';
-                        echo '</nav>';
-                    }
-                    ?>
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<script>
-    function hapusData(idpelanggan) {
-        if (confirm('Apakah Anda yakin ingin menghapus data ini?')) {
-            window.location.href = 'pelangganproses.php?kode=' + idpelanggan + '&proses=proseshapus';
+    <script>
+        function hapusData(idpelanggan) {
+            if (confirm('Apakah Anda yakin ingin menghapus data ini?')) {
+                window.location.href = 'pelangganproses.php?kode=' + idpelanggan + '&proses=proseshapus';
+            }
         }
-    }
-</script>
+
+        function tampilkanGambar(namaGambar) {
+            var gambarPopUp = document.getElementById('gambarPopUp');
+            var modalContent = document.querySelector('.modal-content');
+            var gambarModal = document.getElementById('gambarModal');
+
+            // Set lebar modal sesuai dengan gambar asli
+            var gambarAsli = new Image();
+            gambarAsli.src = namaGambar;
+            gambarAsli.onload = function() {
+                var lebarAsli = this.width;
+                modalContent.style.width = lebarAsli + 'px';
+                gambarModal.src = namaGambar;
+                gambarPopUp.style.display = "block";
+            };
+        }
+
+        function tutupPopUp() {
+            var gambarPopUp = document.getElementById('gambarPopUp');
+            gambarPopUp.style.display = "none";
+        }
+    </script>
