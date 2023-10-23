771f5528b69e57040da4d4b1f29021daf775a643
irumiexe <muhammadilmi2309@gmail.com>
2023-10-23 11:42:24 +0800

    ilmi

--- admin/pelangganinput.php
+++ admin/pelangganinput.php
@@ -113,7 +113,11 @@ $dataPerPage = 10;
                         <input class="form-control mr-1" name="cari" type="search" placeholder="Search" aria-label="Search" value="<?php if (isset($_GET['cari'])) {
                                                                                                                                         echo $_GET['cari'];
                                                                                                                                     } ?>">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <button class="btn" type="submit">
+                            <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </button>
                     </form>
                 </div>
                 <div class="mx-3 table-responsive">
@@ -187,8 +191,7 @@ $dataPerPage = 10;
                                 <span class="close" onclick="tutupPopUp()">&times;</span>
                             </div>
                             <div class="modal-body">
-                                <img id="gambarModal"
-                                >
+                                <img id="gambarModal">
                             </div>
 
                         </div>
