c797218b7e276daad44ab23cfb08ce62e4f777d4
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-10-24 14:28:06 +0800

    ariadi

--- admin/profil.php
+++ admin/profil.php
@@ -74,62 +74,35 @@ if (isset($_SESSION['username'])) {
             <div class="row">
                 <?php foreach ($petlap_data as $petlap) : ?>
                     <div class="col-md-3">
-                        <<<<<<< HEAD <div class="card card-primary card-outline" style="border-top: 1px solid #ccc; margin-bottom: 20px;">
+                        <div class="card card-primary card-outline" style="border-top: 1px solid #ccc; margin-bottom: 20px;">
                             <div class="card-body box-profile">
                                 <div class="text-center">
                                     <img class="profile-user-img img-fluid img-circle" src="../assets/dist/img/user4-128x128.jpg" alt="User profile picture">
-                                    =======
-                                    <div class="card card-primary card-outline">
-                                        <div class="card card-primary card-outline">
-                                            <div class="card-body box-profile">
-                                                <div class="text-center">
-                                                    <img class="profile-user-img img-fluid img-circle" src="../assets/dist/img/user4-128x128.jpg" alt="User profile picture">
-                                                </div>
-
-                                                <h3 class="profile-username text-center">Nina Mcintire</h3>
-
-                                                <p class="text-muted text-center">Software Engineer</p>
-
-                                                <ul class="list-group list-group-unbordered mb-3">
-                                                    <li class="list-group-item">
-                                                        <b>Followers</b> <a class="float-right">1,322</a>
-                                                    </li>
-                                                    <li class="list-group-item">
-                                                        <b>Following</b> <a class="float-right">543</a>
-                                                    </li>
-                                                    <li class="list-group-item">
-                                                        <b>Friends</b> <a class="float-right">13,287</a>
-                                                    </li>
-                                                </ul>
-
-                                                <a href="targetaksi.php?aksi=tambah" class="btn btn-success btn-block"><b>Tambah Target</b></a>
-                                                <a href="#" class="btn btn-primary btn-block"><b>Detail Target</b></a>
-                                                >>>>>>> 682e139c01ff71068dc2b3382173f9b63401240e
-                                            </div>
-
-                                            <h3 class="profile-username text-center"><?php echo $petlap['nama_lengkap']; ?></h3>
-                                            <p class="text-muted text-center"><?php echo $petlap['level']; ?></p>
-
-                                            <ul class="list-group list-group-unbordered mb-3">
-                                                <li class="list-group-item">
-                                                    <b>Followers</b> <a class="float-right">1,322</a>
-                                                </li>
-                                                <li class="list-group-item">
-                                                    <b>Following</b> <a class="float-right">543</a>
-                                                </li>
-                                                <li class="list-group-item">
-                                                    <b>Friends</b> <a class="float-right">13,287</a>
-                                                </li>
-                                            </ul>
-
-                                            <a href="#" class="btn btn-success btn-block"><b>Tambah Target</b></a>
-                                            <a href="#" class="btn btn-primary btn-block"><b>Detail Target</b></a>
-                                        </div>
-                                        <!-- /.card-body -->
-                                    </div>
                                 </div>
-                            <?php endforeach; ?>
+
+                                <h3 class="profile-username text-center"><?php echo $petlap['nama_lengkap']; ?></h3>
+                                <p class="text-muted text-center"><?php echo $petlap['level']; ?></p>
+
+                                <ul class="list-group list-group-unbordered mb-3">
+                                    <li class="list-group-item">
+                                        <b>Followers</b> <a class="float-right">1,322</a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <b>Following</b> <a class="float-right">543</a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <b>Friends</b> <a class="float-right">13,287</a>
+                                    </li>
+                                </ul>
+
+                                <a href="#" class="btn btn-success btn-block"><b>Tambah Target</b></a>
+                                <a href="#" class="btn btn-primary btn-block"><b>Detail Target</b></a>
                             </div>
+                            <!-- /.card-body -->
+                        </div>
                     </div>
+                <?php endforeach; ?>
             </div>
         </div>
+    </div>
+</div>
