4dffe9d2d89e2ddd5c50da347ac169dd4807829a
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-11-02 08:42:08 +0800

    ariadi

--- petlap/header.php
+++ petlap/header.php
@@ -49,7 +49,7 @@ include '../assets/conn/config.php';
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-info">
         <div class="container">
-            <img style="width: 30px; display: inline-block; vertical-align: middle;" src="../img/Logo_PLN.png" alt="Logo">
+            <img style="width: 30px; display: inline-block; vertical-align: middle;" src="../assets/img/Logo_PLN.png" alt="Logo">
             <a class="navbar-brand" href="#">SIPLN</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
