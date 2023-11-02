6ba535afdad326959112624ca52e1c474258b8de
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-11-02 10:56:23 +0800

    ariadi

--- admin/import.php
+++ admin/import.php
@@ -3,30 +3,39 @@ session_start();
 include '../assets/conn/cek.php';
 include '../assets/conn/config.php';
 
-$csv_file = 'mydata.csv';
+if (isset($_FILES['excelFile']) && $_FILES['excelFile']['error'] === UPLOAD_ERR_OK) {
+    $excelFile = $_FILES['excelFile']['tmp_name'];
 
-// Baca file CSV ke dalam array
-$data = array_map('str_getcsv', file($csv_file));
+    // Gunakan library PHPSpreadsheet untuk membaca file Excel
+    require '../vendor/autoload.php'; // Sesuaikan dengan lokasi library
 
-// Loop melalui data dan masukkan ke dalam database
-foreach ($data as $row) {
-    $idpel = mysqli_real_escape_string($db, $row[0]);
-    $kd_akun = mysqli_real_escape_string($db, $row[1]);
-    $tanggal = mysqli_real_escape_string($db, $row[2]);
-    $tanggal_akhir = mysqli_real_escape_string($db, $row[3]);
-    $latitude = mysqli_real_escape_string($db, $row[4]);
-    $longitude = mysqli_real_escape_string($db, $row[5]);
+    // Inisialisasi objek pembaca Excel
+    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($excelFile);
+    $worksheet = $spreadsheet->getActiveSheet();
 
-    // Ganti 'nama_tabel' dengan nama tabel yang ingin Anda gunakan
-    $query = "INSERT INTO tbl_target (idpel, kd_akun, tanggal, tanggal_akhir, latitude, longitude) 
-              VALUES ('$idpel', '$kd_akun', '$tanggal', '$tanggal_akhir', $latitude, $longitude)";
+    // Loop melalui baris-baris dalam file Excel untuk mengimpor data
+    foreach ($worksheet->getRowIterator() as $row) {
+        $rowData = [];
+        foreach ($row->getCellIterator() as $cell) {
+            $rowData[] = $cell->getValue();
+        }
 
-    if (mysqli_query($db, $query)) {
-        echo "Data berhasil diimpor: $idpel, $kd_akun, $tanggal, $tanggal_akhir, $latitude, $longitude<br>";
-    } else {
-        echo "Gagal mengimpor data: " . mysqli_error($db) . "<br>";
+        // Ambil data yang sesuai dari file Excel
+        $idpel = $rowData[0]; // Kolom 1
+        $kd_akun = $rowData[1]; // Kolom 2
+        $rbm = $rowData[2]; // Kolom 3
+        $tanggal = $rowData[3]; // Kolom 4
+        $tanggal_akhir = $rowData[4]; // Kolom 5
+        $latitude = $rowData[5]; // Kolom 6
+        $longitude = $rowData[6]; // Kolom 7
+        $status = $rowData[7]; // Kolom 8
+
+        // Lakukan operasi penyimpanan ke database
+        // Misalnya, menggunakan MySQLi atau PDO
+        $query = "INSERT INTO tbl_target (idpel, kd_akun, rbm, tanggal, tanggal_akhir, latitude, longitude, status) VALUES ('$idpel', '$kd_akun', '$rbm', '$tanggal', '$tanggal_akhir', '$latitude', '$longitude', '$status')";
+
+        // Eksekusi query
+
+        // Di sini, Anda dapat menambahkan validasi dan penanganan kesalahan yang sesuai
     }
 }
-
-// Tutup koneksi
-mysqli_close($db);
